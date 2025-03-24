7fb3a909feadaf7095c22723587f1d7fa2d6d0c3
hinaple <fainthit@kakao.com>
2025-03-24 15:36:50 +0900

    disabled input spell check

--- src/renderer/play/src/webcomponents/repairElement.js
+++ src/renderer/play/src/webcomponents/repairElement.js
@@ -34,6 +34,7 @@ export default class RepairElement extends HTMLElement {
                     element.payload.content;
         } else if (this.type === "input") {
             this.realEl = document.createElement("input");
+            this.realEl.spellcheck = false;
             if (element.payload.placeholder) this.realEl.placeholder = element.payload.placeholder;
             if (element.payload.maxLength !== null)
                 this.realEl.maxlength = element.payload.maxLength;
