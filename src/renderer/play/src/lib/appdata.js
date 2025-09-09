5493100c88484c1b51d7edd5a53b02e10bbbc888
hinaple <fainthit@kakao.com>
2025-09-10 03:50:14 +0900

    fixed sizeRatio util bug

--- src/renderer/play/src/lib/appdata.js
+++ src/renderer/play/src/lib/appdata.js
@@ -44,7 +44,7 @@ export function getAppData() {
 }
 
 export function getSizeRatio() {
-    const ratio = (appdata.config.sizeRatio ?? "1").split(",").map((n) => n);
+    const ratio = (appdata.config.sizeRatio || "1").split(",").map((n) => n);
     return ratio.length === 2 ? ratio : [ratio[0], ratio[0]];
 }
 
