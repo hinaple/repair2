1e2f26507ed3e34f86ec2ac2953a7a440fe1ef28
hinaple <fainthit@kakao.com>
2026-06-05 18:14:01 +0900

    feat: Add report option on main build

--- src/main/electron.vite.config.js
+++ src/main/electron.vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "electron-vite";
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync, rmSync } from "fs";
 import { join } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import pc from "picocolors";
 
 const pkg = JSON.parse(readFileSync(join(__dirname, "../../package.json"), "utf8"));
 const svelteVersion = pkg.dependencies?.svelte ?? pkg.devDependencies?.svelte ?? null;
@@ -11,18 +12,70 @@ const sdkPkg = JSON.parse(
 );
 const sdkVersion = `^${sdkPkg.version}`;
 
+function resolveArgv() {
+    const myArgvFrom = process.argv.indexOf("--");
+    if (myArgvFrom === -1) return {};
+
+    let result = {};
+    let currentKey = null;
+    for (let i = myArgvFrom + 1; i < process.argv.length; i++) {
+        if (process.argv[i].startsWith("--")) {
+            if (currentKey) {
+                result[currentKey] = true;
+                currentKey = null;
+            }
+
+            if (process.argv[i].length > 2) {
+                const temp = process.argv[i].substring(2);
+                if (temp.includes("=")) {
+                    const arr = temp.split("=");
+                    result[arr[0]] = arr[1] ?? "";
+                } else currentKey = temp;
+            }
+        } else if (currentKey) {
+            result[currentKey] = process.argv[i];
+            currentKey = null;
+        }
+    }
+    if (currentKey) result[currentKey] = true;
+
+    return result;
+}
+const argv = resolveArgv();
+
+let plugins = [];
+
+if (argv.report) {
+    const suffix = typeof argv.report === "string" ? argv.report : Date.now();
+    const reportFile = `report_${suffix}.html`;
+    const maxReporters = typeof argv["max-report"] === "string" ? +argv["max-report"] : 5;
+    const reportersDir = join(__dirname, "../../reporters");
+    const reporters = readdirSync(reportersDir);
+    const removingReporters = reporters
+        .filter((f) => f !== reportFile)
+        .sort((a, b) => b.localeCompare(a))
+        .toSpliced(0, maxReporters - 1);
+    for (const r of removingReporters) {
+        rmSync(join(reportersDir, r), { force: true });
+    }
+
+    plugins.push(
+        visualizer({
+            filename: join(reportersDir, reportFile),
+            template: "treemap",
+            gzipSize: true,
+            brotliSize: true
+        })
+    );
+
+    console.log("Report file at " + pc.bgGreen(join(reportersDir, reportFile)));
+}
+
 export default defineConfig({
     main: {
         build: {
             rollupOptions: {
-                plugins: [
-                    visualizer({
-                        filename: "reporter.html",
-                        template: "treemap",
-                        gzipSize: true,
-                        brotliSize: true
-                    })
-                ],
+                plugins,
                 output: {
                     manualChunks: {
                         chokidar: ["chokidar"]
