caf279562b248f21c044ec5b90c4afce4c6efa1c
hinaple <fainthit@kakao.com>
2026-06-12 01:05:14 +0900

    design: Remove x scrollbar in editor sidebar

--- src/renderer/editor/src/sidebar/edits/Edit.svelte
+++ src/renderer/editor/src/sidebar/edits/Edit.svelte
@@ -55,7 +55,7 @@
         flex: 1 1 auto;
         display: flex;
         flex-direction: column;
-        overflow-y: scroll;
+        overflow: hidden scroll;
         padding-block: 20px 70px;
         gap: 15px;
         box-sizing: border-box;
