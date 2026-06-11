ac43082c496c3e1519604880b8bcc213002e8ff9
hinaple <fainthit@kakao.com>
2026-06-11 16:22:09 +0900

    remove/rename: log ui files

--- src/renderer/editor/src/sidebar/log/logs.ts
+++ src/renderer/editor/src/sidebar/log/logs.ts
@@ -1,16 +1,14 @@
 import { Action } from "svelte/action";
 import { Logs, subscribeLog } from "../../lib/logs/logStore";
-import { createLogElement } from "./logLine";
+import { createLogElement } from "./logUI";
 
 const logs: Action<HTMLElement> = (node) => {
     const unsub = subscribeLog(({ type, entry }) => {
-        console.log(type, entry);
         if (type === "update") node.lastElementChild?.remove();
 
         node.append(createLogElement(entry));
     });
 
-    console.log(...Logs.values().map((v) => v.content));
     node.append(...Logs.values().map(createLogElement));
 
     return {
