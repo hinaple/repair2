6aaa9c00ae5fb79824d60c0f75b1505085dead36
SIWON PARK <fainthit@kakao.com>
2026-09-06 16:52:34 +0900

    fix: Store IPC 수정 누락
    #nn

--- src/renderer/play/src/lib/store.ts
+++ src/renderer/play/src/lib/store.ts
@@ -5,8 +5,8 @@ import { registerPluginContextApi } from "./plugin/pluginContext";
 function getStore<T = unknown>(key: string): Promise<T> {
   return ipc.invoke("get-store", key) as Promise<T>;
 }
-function setStore(key: string, value: unknown) {
-  return ipc.send("set-store", key, value);
+function setStore(key: string, value: unknown): Promise<void> {
+  return ipc.invoke("set-store", key, value);
 }
 
 const Api = {
